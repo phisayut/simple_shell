7e7f7ec2a806269d07cc986b48e13655f5987a64
phisayur <fisayoola01@gmail.com>
2023-09-19 04:33:25 -0700

    Simple Shell

--- pfc.c
+++ pfc.c
@@ -1,17 +1,17 @@
 #include "shell.h"
 
-int cant_open(char *file_path);
+int cnt_open(char *file_path);
 int proc_file_commands(char *file_path, int *exe_ret);
 
 /**
- * cant_open - If the file doesn't exist or lacks proper permissions, print
+ * cnt_open - If the file doesn't exist or lacks proper permissions, print
  * a cant open error.
  * @file_path: Path to the supposed file.
  *
  * Return: 127.
  */
 
-int cant_open(char *file_path)
+int cnt_open(char *file_path)
 {
 	char *error, *hist_str;
 	int len;
@@ -64,7 +64,7 @@ int proc_file_commands(char *file_path, int *exe_ret)
 	file = open(file_path, O_RDONLY);
 	if (file == -1)
 	{
-		*exe_ret = cant_open(file_path);
+		*exe_ret = cnt_open(file_path);
 		return (*exe_ret);
 	}
 	line = malloc(sizeof(char) * old_size);
